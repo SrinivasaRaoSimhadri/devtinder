e286dfd42cd200e0bc4d417fb8ee7f31fdd868e2
SrinivasaRaoSimhadri <sinivasaraosimhadri01@gmail.com>
2024-09-18 22:36:14 +0530

    multuple-route-handlers

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,21 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({name: "Srinu"});
-})
+// app.use("/route", rh1, [rh2], [rh3, rh4], rh5, [rh6]);
 
-app.post("/user", (req, res) => {
-    res.send("user details saved successfully!");
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("user details deleted successfully!");
-})
-
-app.use("/test", (req, res) => {
-    res.send("this is the hello from the /test!");
-})
+app.use("/user", (req, res, next)=>{
+    console.log("controller 1");
+    next();
+    // res.send("route handler 1"); 
+},
+(req, res, next) =>{
+    console.log("controller 2");
+    // res.send("route habdler 2");
+    next();
+},
+(req, res, next)=>{
+    console.log("controller 3");
+    // res.send("route handler 3");
+    next();
+},
+(req, res, next)=> {
+    console.log("controller 4");
+    res.send("route handler 4");
+}
+)
 
 app.listen(3000, ()=> {
     console.log("The server is running at port 3000 successfully")
