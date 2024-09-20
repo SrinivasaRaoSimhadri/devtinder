6a11ecbdfe6ac8bac64cf089b6dbcf65197b644f
SrinivasaRaoSimhadri <sinivasaraosimhadri01@gmail.com>
2024-09-20 11:34:08 +0530

    route-chaining

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,28 +2,23 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/route", rh1, [rh2], [rh3, rh4], rh5, [rh6]);
+app.use("/", (req, res, next)=>{
+    // res.send("this is a response 1");
+    console.log("1");
+    next();
+})
 
-app.use("/user", (req, res, next)=>{
-    console.log("controller 1");
-    next();
-    // res.send("route handler 1"); 
-},
-(req, res, next) =>{
-    console.log("controller 2");
-    // res.send("route habdler 2");
+app.get("/user", (req, res, next) => {
+    console.log("2");
     next();
 },
-(req, res, next)=>{
-    console.log("controller 3");
-    // res.send("route handler 3");
+(req, res, next) => {
+    console.log("3");
     next();
 },
-(req, res, next)=> {
-    console.log("controller 4");
-    res.send("route handler 4");
-}
-)
+(req, res, next) => {
+    res.send("this is a reponse 2");
+})
 
 app.listen(3000, ()=> {
     console.log("The server is running at port 3000 successfully")
